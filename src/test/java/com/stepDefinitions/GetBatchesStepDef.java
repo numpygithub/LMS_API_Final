d9f8682531cacca78596993200c2354dec5c5a96
swatisbci <swatisbci@gmail.com>
2023-01-13 12:10:54 -0600

    First commit for Get requests

--- src/test/java/com/stepDefinitions/GetBatchesStepDef.java
+++ src/test/java/com/stepDefinitions/GetBatchesStepDef.java
@@ -1,5 +1,35 @@
 package com.stepDefinitions;
 
+import org.testng.Assert;
+
+import com.utils.Base;
+import com.utils.Loggerload;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.Method;
+
 public class GetBatchesStepDef {
+	
+	private Base base;
+	public GetBatchesStepDef(Base base) {
+		this.base = base;
+		
+	}
+	
+	@When("User send api request to get all batches with {string}")
+	public void user_send_api_request_to_get_all_batches_with(String url) {
+		
+		Loggerload.info("In user_send_api_request_to_get_all_batches_with");
+		 base.response = base.requestSpecification.request(Method.GET,url);
+	}
+	
+	@Then("user validate response body batch name is {string}")
+	public void user_validate_response_body_batch_name_is(String name) {
+		String responseBody = base.response.getBody().asString();
+		String actualName = base.response.then().extract().path("batchName");
+		Assert.assertEquals(actualName, name);
+		Loggerload.info("---------->Actual response status is  :" + actualName);
+	}
 
 }
--- src/test/java/com/utils/Base.java
+++ src/test/java/com/utils/Base.java
@@ -6,15 +6,15 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class Utilities {
+public class Base {
 	public final String baseURL = "https://lms-backend-service.herokuapp.com/lms";
 	public  Response response;
 	public Scenario scenario;
 	public RequestSpecification requestSpecification;
 	
-	public Utilities(){
+	public Base(){
+		RestAssured.baseURI = baseURL;
 		requestSpecification = RestAssured.given();
-		
 	}
 	
 
