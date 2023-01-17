0c66ce29e3c184ce61a0a0fab51204a9e3117d9c
swatisbci <swatisbci@gmail.com>
2023-01-17 10:25:48 -0600

    FINAL COMMIT

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",glue ={"com.stepDefinitions"},monochrome = true,
-plugin= {"pretty","html:target/cucumber-report/cucumber.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, tags =("@s_002 or s_001"))
+plugin= {"pretty","html:target/cucumber-report/cucumber.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}) //tags =("@s_002 or s_001"))
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
